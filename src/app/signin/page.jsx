b1a552defdf2a6b0ab9619822301fe058e49d87c
Your Name <babulhossan.info@gmail.com>
2026-04-30 22:25:54 +0600

    deploy error

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,93 +1,93 @@
-// "use client";
+"use client";
 
-// import { authClient } from "@/lib/auth-client";
-// import { Button } from "@heroui/react";
-// import React from "react";
-// import { useForm } from "react-hook-form";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@heroui/react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-// const LoginPage = () => {
-//     const {
-//         register,
-//         handleSubmit,
-//         formState: { errors },
-//     } = useForm();
+const LoginPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-//     const handleLogin = async (data) => {
-//         console.log(data);
+    const handleLogin = async (data) => {
+        console.log(data);
 
-//         try {
-//             const { data: res, error } = await authClient.signIn.email({
-//                 email: data.email,
-//                 password: data.password,
-//                 callbackURL: "/",
-//             });
+        try {
+            const { data: res, error } = await authClient.signIn.email({
+                email: data.email,
+                password: data.password,
+                callbackURL: "/",
+            });
 
-//             if (error) {
-//                 alert(error.message);
-//                 return;
-//             }
+            if (error) {
+                alert(error.message);
+                return;
+            }
 
-//             if (res) {
-//                 alert("Login SUCCESS ✅");
-//             }
+            if (res) {
+                alert("Login SUCCESS ✅");
+            }
 
-//             console.log(res);
-//         } catch (err) {
-//             console.error(err);
-//             alert("Something went wrong ❌");
-//         }
-//     };
+            console.log(res);
+        } catch (err) {
+            console.error(err);
+            alert("Something went wrong ❌");
+        }
+    };
 
-//     return (
-//         <div className="mt-5 flex items-center justify-center bg-base-200 px-4 min-h-screen">
-//             <form
-//                 onSubmit={handleSubmit(handleLogin)}
-//                 className="w-full max-w-md bg-base-100 shadow-md rounded-xl p-6"
-//             >
-//                 <h1 className="text-2xl font-bold mb-6 text-center">
-//                     Sign In Your Account
-//                 </h1>
-
-
-//                 <label className="text-sm font-semibold">Email</label>
-//                 <input
-//                     type="email"
-//                     className="input input-bordered w-full mt-1 mb-2"
-//                     placeholder="Enter your email"
-//                     {...register("email", { required: "Email is required" })}
-//                 />
-//                 {errors.email && (
-//                     <p className="text-red-500 text-sm mb-2">
-//                         {errors.email.message}
-//                     </p>
-//                 )}
+    return (
+        <div className="mt-5 flex items-center justify-center bg-base-200 px-4 min-h-screen">
+            <form
+                onSubmit={handleSubmit(handleLogin)}
+                className="w-full max-w-md bg-base-100 shadow-md rounded-xl p-6"
+            >
+                <h1 className="text-2xl font-bold mb-6 text-center">
+                    Sign In Your Account
+                </h1>
 
 
-//                 <label className="text-sm font-semibold">Password</label>
-//                 <input
-//                     type="password"
-//                     className="input input-bordered w-full mt-1 mb-2"
-//                     placeholder="Enter your password"
-//                     {...register("password", {
-//                         required: "Password is required",
-//                         minLength: {
-//                             value: 6,
-//                             message: "Password must be at least 6 characters",
-//                         },
-//                     })}
-//                 />
-//                 {errors.password && (
-//                     <p className="text-red-500 text-sm mb-2">
-//                         {errors.password.message}
-//                     </p>
-//                 )}
+                <label className="text-sm font-semibold">Email</label>
+                <input
+                    type="email"
+                    className="input input-bordered w-full mt-1 mb-2"
+                    placeholder="Enter your email"
+                    {...register("email", { required: "Email is required" })}
+                />
+                {errors.email && (
+                    <p className="text-red-500 text-sm mb-2">
+                        {errors.email.message}
+                    </p>
+                )}
 
-//                 <Button type="submit" variant="secondary" className="w-full mt-5">
-//                     Login
-//                 </Button>
-//             </form>
-//         </div>
-//     );
-// };
 
-// export default LoginPage;
+                <label className="text-sm font-semibold">Password</label>
+                <input
+                    type="password"
+                    className="input input-bordered w-full mt-1 mb-2"
+                    placeholder="Enter your password"
+                    {...register("password", {
+                        required: "Password is required",
+                        minLength: {
+                            value: 6,
+                            message: "Password must be at least 6 characters",
+                        },
+                    })}
+                />
+                {errors.password && (
+                    <p className="text-red-500 text-sm mb-2">
+                        {errors.password.message}
+                    </p>
+                )}
+
+                <Button type="submit" variant="secondary" className="w-full mt-5">
+                    Login
+                </Button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginPage;
